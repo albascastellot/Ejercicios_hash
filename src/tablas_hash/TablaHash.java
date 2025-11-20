a66637ff94e8bd2cd17454d3f333a351447a292f
albascastellot <alba.scastellot@alumnos.upm.es>
2025-11-20 14:41:46 +0100

    Update TablaHash.java

--- src/tablas_hash/TablaHash.java
+++ src/tablas_hash/TablaHash.java
@@ -24,15 +24,5 @@ public class TablaHash {
 	
 	//LO QUE HEMOS HECHO EN ESTE EJERCICIO CON ESTAS 2 TABLAS HASH ES EL FUNCIONAMIENTO INTERNO DE UN MAP
 	//EL FUNCIONAMIENTO INTERNO DE UN SET TENDRÍA SOLO UNA TABLA
-	/**
-	 * ¿pero y cuando hay que hacer esto, si ya existen los map y los set que lo hacen por nosotros?
-	 * bueno pues para programitas y cosas sin importancia o mierderos pues puedes usarte esas colecciones predefinidas
-	 * pero en esta asignatura estamos aprendiendo a ver como funcionan las cosas por dentro (los algoritmos)
-	 * para aprender a programarlos nosotros mismos cuando nos haga falta porque cuando tengamos que hacer un programa
-	 * tocho, importante y que tenga que ser muy pro, nos interesará hacernos nosotros mismos las tablas hash porque
-	 * de esa manera tú puedes tener absolutamente todo controlado (tamaño de la tabla, posiciones y de todo)
-	 * mientras que si usas los map o los set pues eso lo hace internamente eclipse y tu no te enteras de nada y no
-	 * puedes controlarlo segun tus necesidades.
-	 */
 	
 }
